c9c4a6f9bdba9316ba56be50b8328e518b8b954d
mgispert <mcg-511@hotmail.com>
2021-10-10 15:21:38 +0200

    feat: fix indentation

--- src/components/TaskBar/TaskBar.js
+++ src/components/TaskBar/TaskBar.js
@@ -1,24 +1,25 @@
-import {
-    FormLabel,
-    Input,
-    Button
-} from "@chakra-ui/react"
+import { FormLabel, Input, Button } from "@chakra-ui/react";
 import { useState } from "react";
 
 export const TaskBar = (props) => {
-    const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.onAddTask(inputValue);
-        setInputValue('');
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onAddTask(inputValue);
+    setInputValue("");
+  };
 
-    return (
-        <form id="text" onSubmit={handleSubmit}>
-            <FormLabel>Write a task to add!</FormLabel>
-            <Input type="text" value={inputValue} placeholder={'Add your task here'} onChange={(e) => setInputValue(e.target.value)} />
-            <Button type='submit'>Add Task</Button>
-        </form>
-    );
-}
+  return (
+    <form id="text" onSubmit={handleSubmit} data-testid="taskBar">
+      <FormLabel>Write a task to add!</FormLabel>
+      <Input
+        type="text"
+        value={inputValue}
+        placeholder={"Add your task here"}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <Button type="submit">Add Task</Button>
+    </form>
+  );
+};
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,25 +1,43 @@
-import { List, ListItem, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
-
+import {
+  List,
+  ListItem,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
 
 export const TaskList = (props) => {
-    return (
-        <Tabs>
-            <TabList>
-                <Tab>To Do</Tab>
-                <Tab>Done</Tab>
-            </TabList>
-            <TabPanels>
-                <TabPanel>
-                    <List spacing={3}>
-                        {props.toDoTasks && props.toDoTasks.map((value, index) => <ListItem key={index} onClick={() => props.onClickToDoTask(value)}>{value}</ListItem>)}
-                    </List>
-                </TabPanel>
-                <TabPanel>
-                    <List spacing={3}>
-                        {props.doneTasks && props.doneTasks.map((value, index) => <ListItem key={index}>{value}</ListItem>)}
-                    </List>
-                </TabPanel>
-            </TabPanels>
-        </Tabs>
-    );
-}
+  return (
+    <Tabs data-testid="taskList">
+      <TabList>
+        <Tab>To Do</Tab>
+        <Tab>Done</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <List spacing={3}>
+            {props.toDoTasks &&
+              props.toDoTasks.map((value, index) => (
+                <ListItem
+                  key={index}
+                  onClick={() => props.onClickToDoTask(value)}
+                >
+                  {value}
+                </ListItem>
+              ))}
+          </List>
+        </TabPanel>
+        <TabPanel>
+          <List spacing={3}>
+            {props.doneTasks &&
+              props.doneTasks.map((value, index) => (
+                <ListItem key={index}>{value}</ListItem>
+              ))}
+          </List>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
