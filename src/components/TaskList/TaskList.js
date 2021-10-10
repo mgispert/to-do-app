1a2a8f8355641f4daf293956e2dbac0070e3d32d
mgispert <mcg-511@hotmail.com>
2021-10-10 13:03:55 +0200

    feat: create tasklist component

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,25 @@
+import { List, ListItem, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+
+
+export const TaskList = (props) => {
+    return (
+        <Tabs>
+            <TabList>
+                <Tab>To Do</Tab>
+                <Tab>Done</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <List spacing={3}>
+                        {props.toDoTasks && props.toDoTasks.map((value, index) => <ListItem key={index} onClick={() => props.onClickToDoTask(value)}>{value}</ListItem>)}
+                    </List>
+                </TabPanel>
+                <TabPanel>
+                    <List spacing={3}>
+                        {props.doneTasks && props.doneTasks.map((value, index) => <ListItem key={index}>{value}</ListItem>)}
+                    </List>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+    );
+}
