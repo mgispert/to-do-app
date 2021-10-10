00bba905ce9a4c848da0b4d27c4ab1f1aa950cb3
mgispert <mcg-511@hotmail.com>
2021-10-10 10:53:01 +0200

    feat: create taskbar component

---
+++ src/components/TaskBar/TaskBar.js
@@ -0,0 +1,23 @@
+import {
+    FormLabel,
+    Input,
+    Button
+} from "@chakra-ui/react"
+import { useState } from "react";
+
+export const TaskBar = (props) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.onAddTask(inputValue);
+    }
+
+    return (
+        <form id="text" onSubmit={handleSubmit}>
+            <FormLabel>Write a task to add!</FormLabel>
+            <Input type="text" value={inputValue} placeholder={'Add your task here'} onChange={(e) => setInputValue(e.target.value)} />
+            <Button type='submit'>Add Task</Button>
+        </form>
+    );
+}
